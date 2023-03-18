cebe35749a8fe770d770dcc86188b51781f9b6a7
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-03-18 00:15:34 +0000

    added code to make night-mode possible

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useContext } from 'react'
 import './App.css'
 import Particle from './components/Particle'
+import { ThemeContext } from './theme/Theme'
+import Navbar from './components/Navbar/Navbar'
+import { Container } from "react-bootstrap";
 
 function App() {
+  const [{ themeName }] = useContext(ThemeContext)
   return (
-    <div className="App">
+    <div id='top' className={`${themeName} app`}>
+      <Container fluid className="main" id="main">
       <Particle />
+      <Container className="main-content">
+      <Navbar />
+      </Container>
+      </Container>
     </div>
   )
 }
