21be9b0f663550bf92a217baca7993dda740a860
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-04-12 12:46:42 +0200

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import { useContext } from 'react'
 import './App.css'
-import Particle from './components/Particle'
 import { ThemeContext } from './theme/Theme'
-import Navbar from './components/Navbar/Navbar'
-import { Container } from "react-bootstrap";
 
 function App() {
   const [{ themeName }] = useContext(ThemeContext)
   return (
     <div id='top' className={`${themeName} app`}>
-      <Container fluid className="main" id="main">
-      <Particle />
-      <Container className="main-content">
-      <Navbar />
-      </Container>
-      </Container>
     </div>
   )
 }
